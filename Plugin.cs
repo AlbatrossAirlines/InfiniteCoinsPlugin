3e7468ceb3a926776847d67594faa95f62898226
AlbatrossAirlines <sarsparilla.automata@gmail.com>
2022-09-17 13:08:36 -0600

    Eliminate anim delay

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,30 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using HarmonyLib;
 
 namespace InfiniteCoins;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class ExplosiveCoins : BaseUnityPlugin
+public class InfiniteCoins : BaseUnityPlugin
 {
     private static Harmony _harmony;
     
     private void Awake()
     {
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        _harmony = Harmony.CreateAndPatchAll(typeof(InfiniteCoins));
+        _harmony = new Harmony(string.Format("harmony-auto-{0}", (object)Guid.NewGuid()));
+        
+        var updatePatch = typeof(InfiniteCoinPatches).GetMethod("Update");
+        _harmony.Patch(
+            AccessTools.Method(typeof(Revolver), "Update"),
+            postfix: new HarmonyMethod(updatePatch)
+        );
+        
+        var refreshPatch = typeof(InfiniteCoinPatches).GetMethod("RefreshWeaponCharges");
+        _harmony.Patch(
+            AccessTools.Method(typeof(WeaponCharges), "Charge"),
+            postfix: new HarmonyMethod(refreshPatch)
+        );
     }
 
     private void OnDestroy()
@@ -20,13 +33,18 @@ public class ExplosiveCoins : BaseUnityPlugin
     }
 }
 
-[HarmonyPatch(typeof(WeaponCharges), nameof(WeaponCharges.Charge))]
-class InfiniteCoins
+class InfiniteCoinPatches
 {
     private const float MAX_COIN_CHARGE = 400.0f;
-    
-    [HarmonyPostfix]
-    public static void RefreshCoinCharges(float _, ref WeaponCharges __instance)
+
+    // Patch update to eliminate animation delay
+    public static void Update(ref Revolver __instance)
+    {
+        __instance.pierceCharge = 10000.0f; // Arbitrary large number
+    }
+
+    // Patch charge to always refresh coins to max
+    public static void RefreshWeaponCharges(float amount, ref WeaponCharges __instance)
     {
         __instance.rev1charge = MAX_COIN_CHARGE;
     }
